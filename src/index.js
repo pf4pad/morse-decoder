669a39316d6c2c99149438233794c442ae9f9df7
pf4pad <65763670+pf4pad@users.noreply.github.com>
2022-01-26 20:27:53 +0200

    morse

--- src/index.js
+++ src/index.js
@@ -38,7 +38,61 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   const checkString = function (expr) {
+        let round
+        if (expr.length % 10 === 0) {
+            return expr
+        } else {
+            round = Math.ceil(expr.length / 10) * 10;
+            return expr.padStart(round, '0')
+        }
+    }
+
+    function getArrayTen(expr) {
+
+        let newStr = checkString(expr)
+
+        let arr = []
+        for (let i = 0; i < newStr.length; i = i + 10) {
+            arr.push(newStr.slice(i, i + 10))
+        }
+
+        let arr3 = arr.map(el => el.split('00'))
+        let arr4 = []
+        for (let i = 0; i < arr3.length; i++) {
+            arr4[i] = arr3[i].filter(el => el !== '')
+        }
+        let arr5 = []
+        for (let i = 0; i < arr4.length; i++) {
+            for (let k = 0; k < arr4[i].length; k++) {
+                arr5.push(arr4[i][k].match(/.{1,2}/g))
+            }
+        }
+
+        let srtFinish = '';
+        const objLetter = {
+            '11': '-',
+            '10': '.',
+            '**': ' '
+        }
+        for (let i = 0; i < arr5.length; i++) {
+            srtFinish += ' '
+            for (let k = 0; k < arr5[i].length; k++) {
+                srtFinish += objLetter[arr5[i][k]]
+            }
+        }
+        return srtFinish
+    }
+    let morseCode = getArrayTen(expr)
+    const decodeLetter = letter => {
+        return MORSE_TABLE[letter];
+    }
+
+    const decodeWord = word => {
+        return word.split(' ').map(decodeLetter).join('');
+    }
+
+    return morseCode.trim().split('      ').map(decodeWord).join(' ');
 }
 
 module.exports = {
